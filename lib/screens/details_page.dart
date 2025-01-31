67287ede1a34b129e40dd52563e690527846cb62
aunshpatel <patelaunsh28@gmail.com>
2025-01-31 22:28:32 +0530

    Confetti, and some const data addition

--- lib/screens/details_page.dart
+++ lib/screens/details_page.dart
@@ -1,9 +1,13 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:photo_view/photo_view.dart';
 import '../blocs/pet_block.dart';
 import '../models/pet_model.dart';
+import 'package:confetti/confetti.dart';
 
+import '../widgets/consts.dart';
 
 class DetailsPage extends StatefulWidget {
   final Pet pet;
@@ -14,8 +18,60 @@ class DetailsPage extends StatefulWidget {
   State<DetailsPage> createState() => _DetailsPageState();
 }
 
-class _DetailsPageState extends State<DetailsPage> {
-  bool _isAdopting = false; // Track adoption state
+class _DetailsPageState extends State<DetailsPage> with SingleTickerProviderStateMixin {
+  bool _isAdopted = false;
+  late AnimationController _controller;
+  late Animation<double> _animation;
+  late ConfettiController _confettiController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 2000),
+      vsync: this,
+    );
+    _animation = CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeInOut,
+    );
+
+    _confettiController = ConfettiController(
+      duration: const Duration(seconds: 3),
+    );
+  }
+
+  /*Path drawStar(Size size) {
+    // Method to convert degrees to radians
+    double degToRad(double deg) => deg * (pi / 180.0);
+
+    const numberOfPoints = 5;
+    final halfWidth = size.width / 2;
+    final externalRadius = halfWidth;
+    final internalRadius = halfWidth / 2.5;
+    final degreesPerStep = degToRad(360 / numberOfPoints);
+    final halfDegreesPerStep = degreesPerStep / 2;
+    final path = Path();
+    final fullAngle = degToRad(360);
+    path.moveTo(size.width, halfWidth);
+
+    for (double step = 0; step < fullAngle; step += degreesPerStep) {
+      path.lineTo(halfWidth + externalRadius * cos(step),
+          halfWidth + externalRadius * sin(step));
+      path.lineTo(halfWidth + internalRadius * cos(step + halfDegreesPerStep),
+          halfWidth + internalRadius * sin(step + halfDegreesPerStep));
+    }
+    path.close();
+    return path;
+  }*/
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    _confettiController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,69 +79,124 @@ class _DetailsPageState extends State<DetailsPage> {
       appBar: AppBar(
         title: Text('Pet Details'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: SizedBox(
-          width: MediaQuery.of(context).size.width,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Hero(
-                tag: 'petImage - ${widget.pet.name}',
-                child: ClipRRect(
-                  child:SizedBox(
-                    height: 300,
-                    child: PhotoView(
-                      imageProvider: AssetImage(widget.pet.image),
-                      backgroundDecoration: BoxDecoration(color: Colors.transparent),
-                      minScale: PhotoViewComputedScale.contained * 1,
-                      maxScale: PhotoViewComputedScale.covered * 2.0,
-                    ),
-                  )
-                ),
-              ),
-              const SizedBox(height: 16.0),
-              Text('${widget.pet.name}', style: const TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold)),
-              Text('${widget.pet.age} years old', style: const TextStyle(fontSize: 18.0)),
-              Text('\$${widget.pet.price}', style: const TextStyle(fontSize: 18.0)),
-              const SizedBox(height: 16.0),
-              ElevatedButton(
-                onPressed: _isAdopting ? null : () async {
-                  setState(() {
-                    _isAdopting = true;
-                  });
-
-                  // Dispatch AdoptPet event with the correct index
-                  // only if the index is valid
-                  if (context.read<PetBloc>().state is PetsLoaded) {
-                    final currentState = context.read<PetBloc>().state as PetsLoaded;
-                    if (currentState.pets.any((element) => element == widget.pet)) {
-                      final index = currentState.pets.indexOf(widget.pet);
-                      context.read<PetBloc>().add(AdoptPet(index));
-                    } else {
-                      // Handle the case where the pet is not found in the current state
-                      // (e.g., show an error message)
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(
-                          content: Text('Pet not found.'),
+      body: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(25, 5, 25, 5),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Hero(
+                  tag: 'petImage - ${widget.pet.name}',
+                  child: ClipRRect(
+                      child:SizedBox(
+                        height: 300,
+                        child: PhotoView(
+                          imageProvider: AssetImage(widget.pet.image),
+                          backgroundDecoration: BoxDecoration(color: Colors.transparent),
+                          minScale: PhotoViewComputedScale.contained * 1,
+                          maxScale: PhotoViewComputedScale.covered * 2.0,
                         ),
-                      );
-                    }
-                  }
+                      )
+                  ),
+                ),
+                const SizedBox(height: 16.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('Name:', style: const TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold)),
+                    Text(widget.pet.name, style: const TextStyle(fontSize: 20.0, fontWeight: FontWeight.w500)),
+                  ],
+                ),
+                const SizedBox(height: 16.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('Animal Type:', style: const TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold)),
+                    Text(widget.pet.animalType, style: const TextStyle(fontSize: 20.0, fontWeight: FontWeight.w500)),
+                  ],
+                ),
+                const SizedBox(height: 16.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('Breed:', style: const TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold)),
+                    Text(widget.pet.breed, style: const TextStyle(fontSize: 20.0, fontWeight: FontWeight.w500)),
+                  ],
+                ),
+                const SizedBox(height: 16.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('Age:', style: const TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold)),
+                    Text('${widget.pet.age} years old', style: const TextStyle(fontSize: 20.0, fontWeight: FontWeight.w500)),
+                  ],
+                ),
+                const SizedBox(height: 16.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('Price(in USD):', style: const TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold)),
+                    Text('\$${widget.pet.price}', style: const TextStyle(fontSize: 20.0, fontWeight: FontWeight.w500)),
+                  ],
+                ),
+                const SizedBox(height: 30.0),
+                Material(
+                  color: Color(0XFF697489),
+                  borderRadius: BorderRadius.circular(30.0),
+                  child: MaterialButton(
+                    minWidth: 150.0,
+                    height: 60.0,
+                    child: _isAdopted ? const Text('Adopted',  style: const TextStyle(fontSize: 20.0, fontWeight: FontWeight.w500, color: Colors.white)) : const Text('Adopt Me',  style: const TextStyle(fontSize: 20.0, fontWeight: FontWeight.w500, color: Colors.white)),
+                    onPressed: _isAdopted ? null : () async {
+                      setState(() {
+                        _isAdopted = true;
+                        _confettiController.play();
+                      });
 
-                  // Simulate a short delay for demonstration
-                  await Future.delayed(const Duration(seconds: 1));
+                      // Dispatch AdoptPet event with the correct index
+                      // only if the index is valid
+                      if (context.read<PetBloc>().state is PetsLoaded) {
+                        final currentState = context.read<PetBloc>().state as PetsLoaded;
+                        if (currentState.pets.any((element) => element == widget.pet)) {
+                          final index = currentState.pets.indexOf(widget.pet);
+                          context.read<PetBloc>().add(AdoptPet(index));
+                        } else {
+                          // Handle the case where the pet is not found in the current state
+                          // (e.g., show an error message)
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                              content: Text('Pet not found.'),
+                            ),
+                          );
+                        }
+                      }
 
-                  setState(() {
-                    _isAdopting = false;
-                  });
-                },
-                child: _isAdopting ? const CircularProgressIndicator() : const Text('Adopt Me'),
-              ),
-            ],
+                      // Simulate a short delay for demonstration
+                      await Future.delayed(const Duration(seconds: 1));
+
+                      commonAlertBox(context, 'Adoption Successful!', 'You have adopted ${widget.pet.name}!');
+                    },
+                  ),
+                )
+              ],
+            ),
           ),
-        ),
+          Align(
+            alignment: Alignment.topCenter,
+            child: ConfettiWidget(
+              confettiController: _confettiController,
+              blastDirectionality: BlastDirectionality.explosive,
+              // blastDirection: pi / 2,
+              shouldLoop: true,
+              // createParticlePath: drawStar,
+              emissionFrequency: 0, // how often it should emit
+              numberOfParticles: 500, // number of particles to emit in one emission
+              gravity: 0.1, // gravity - or fall speed
+            ),
+          )
+        ],
       ),
     );
   }
---
+++ lib/widgets/consts.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+const kDarkTitleColor = Color(0XFF3A4355);
+
+const kLightTitleColor = Color(0XFF697489);
+
+const kSideMenuDarkTextStyle = TextStyle(
+  fontWeight: FontWeight.bold,
+  fontSize: 20,
+  color: kDarkTitleColor,
+);
+
+const kLightSemiBoldTextStyle = TextStyle(
+    color: kLightTitleColor,
+    fontSize: 18,
+    fontWeight:FontWeight.w500
+);
+
+
+
+Future<void> commonAlertBox(BuildContext context, String title, String message)  {
+  return showDialog<void>(
+    context: context,
+    barrierDismissible: false,
+    builder: (BuildContext context) {
+      return AlertDialog.adaptive(
+        title: Text(title, style: kSideMenuDarkTextStyle),
+        content: Text(message, style: kLightSemiBoldTextStyle),
+        actions: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              TextButton(
+                child: const Text('OK', style: kLightSemiBoldTextStyle),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          ),
+        ],
+      );
+    },
+  );
+}
